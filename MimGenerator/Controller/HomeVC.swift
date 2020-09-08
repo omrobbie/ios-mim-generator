738cf40da5b4935753c463f7f0259af754bb177f
omrobbie <putu.roby@yahoo.com>
2020-09-08 12:12:42 +0800

    create collection view for image list and make it grid with 3 columns

---
+++ MimGenerator/Controller/HomeVC.swift
@@ -0,0 +1,44 @@
+//
+//  HomeVC.swift
+//  MimGenerator
+//
+//  Created by omrobbie on 08/09/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    @IBOutlet weak var imageList: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: - Handle imageList layout
+extension HomeVC: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenWidth = UIScreen.main.bounds.width
+        let scaleFactor = (screenWidth / 3) - 6
+        return CGSize(width: scaleFactor, height: scaleFactor)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .zero
+    }
+}
+
+// MARK: - Handle imageList data
+extension HomeVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemRed
+        return cell
+    }
+}
